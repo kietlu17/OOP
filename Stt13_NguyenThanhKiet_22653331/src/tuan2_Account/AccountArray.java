da87d065baf3ed031441e0c49528aa15e7399a8a
kietlu17 <kietnguyen17042004@gmail.com>
2023-09-14 16:37:49 +0700

    modify accountarray.java

--- Stt13_NguyenThanhKiet_22653331/src/tuan2_Account/AccountArray.java
+++ Stt13_NguyenThanhKiet_22653331/src/tuan2_Account/AccountArray.java
@@ -79,7 +79,7 @@ public class AccountArray {
 		for (int i=0; i<n-1;i++) {
 			for(int j=0;j<n-i-1;j++) {
 				if (sorted[j].getSoTienTrongTK()>sorted[j].getSoTienTrongTK()) {
-					
+					//tstsa
 			}
 		}
 	}
